f442c7749bc775bb209135bc21cc4b8ca8b60957
nico <nico.sasse@cofinpro.de>
2018-10-22 15:55:15 +0200

    Vermögen optimiert

--- src/js/Vermoegen.js
+++ src/js/Vermoegen.js
@@ -1,5 +1,6 @@
 
 let boxList = document.getElementsByClassName('amount');
+let boxCounter = 1;
 
 function calculateSum() {
 
@@ -15,24 +16,32 @@ function calculateSum() {
 }
 
 function removeBox() {
-    /*Box entfernen*/
+    let boxReset = "";
+    document.getElementById("inputbox").innerHTML = boxReset;
 }
 
 function addBox() {
     /*ID vergabe muss noch ergänzt werden*/
+    boxCounter++;
+
+    let input = document.createElement('div');
+    input.class = 'row';
+    input.id = 'box' + boxCounter;
+    document.getElementById('area').appendChild(input);
+
     let box = '<div class="row">' +
         '<div class="col" id="sidebar"></div>' +
         '<div class="col form-group inputbox">' +
         '<input id="description" type="text" class="form-control textfield" placeholder="Beschreibung" value="">' +
-        '<input id="amount" type="number" class="form-control  textfield amount" placeholder="Betrag" value="" min="0" onfocusout="calculateSum()">' +
+        '<input id="amount" type="number" class="form-control  textfield amount" placeholder="Betrag" value="" min="0" oninput="calculateSum()">' +
         '</div>' +
         '<div class="col inputbox" id="deletebutton">' +
-        '<img src="../ressources/img/Icons/minusIcon.png" width="100%">' +
+        '<img src="../ressources/img/Icons/minusIcon.png" width="100%" onclick="removeBox()">' +
         '</div>' +
         '<div class="col" id="sidebar"></div>' +
         '</div>' +
         '<div id="boxes"></div>' +
         '</div>';
 
-        document.getElementById('boxes').innerHTML = box;
+    document.getElementById('box' + boxCounter).innerHTML = box;
 }
