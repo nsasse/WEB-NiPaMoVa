a05013945f617dbee90d3d0e573792f914327560
nico <nico.sasse@cofinpro.de>
2018-10-23 22:11:40 +0200

    Kontakte.js erweitern (Teamarbeit)
    Co-Authored-By: vannibrandl <vannibrandl@users.noreply.github.com>
    Co-Authored-By: patwade <patwade@users.noreply.github.com>
    Co-Authored-By: monibichlmaier <monibichlmaier@users.noreply.github.com>

--- src/js/Kontakt.js
+++ src/js/Kontakt.js
@@ -1,4 +1,16 @@
 
-function submit(){
-//Übermitteln von Daten
+function submit() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    //In Datenbank schreiben oder E-Mail an uns versenden
+
+    console.log(name + email + message)
+    if (name == "" || email == "" || message == "") {
+        window.alert("Bitte überprüfen Sie Ihre Eingaben!");
+    }
+    else {
+        window.alert("Vielen Dank für Ihre Nachricht. Wir melden uns in kürze.");
+    }
 }
