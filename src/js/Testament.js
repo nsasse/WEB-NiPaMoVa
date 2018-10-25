d54a1e6fe9a67bc573ae633c5b21257ffe5db88c
Patrick Wade <patrick.wade@cofinpro.de>
2018-10-25 15:14:28 +0200

    fortschritt zusammenfassung

--- src/js/Testament.js
+++ src/js/Testament.js
@@ -1,7 +1,30 @@
-let test = {
-    constructor(firstName, lastName) {
-        this.firsName = firstName;
+let Testament = class {
+    constructor(firstName,lastName,email) {
+        this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
+}
+
+let Vermoegen = class {
+    constructor(value,description) {
+        this.value = value;
+        this.description = description;
+    }
+}
+
+let testament = new Testament('Peter','Mayer','Peter.Mayer@gmx.de');
+
+function loadTestamentData() {
+    //Persönliche Daten
+    document.getElementById("vorname").innerHTML = testament.firstName;
+    document.getElementById("nachname").innerHTML = testament.lastName;
+    document.getElementById("email").innerHTML = testament.email;
+
+    //Vermögen
+    
+
+    //Verbindlichkeiten
+
+    console.log("loaded");
 }
