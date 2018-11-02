4e1e8fb9ed6636dcc3b047cfe4a71f6a13c92f69
moni bichlmaier <monika-bichlmaier@web.de>
2018-11-02 17:04:32 +0100

    Verteilung angepasst

--- src/js/Verteilung.js
+++ src/js/Verteilung.js
@@ -2,19 +2,24 @@
 let jaButton = document.getElementById("ja");
 let neinButton = document.getElementById("nein");
 
-let ehe = document.getElementById("eheg");
-
 
 eheverteilen();
-function eheverteilen(){
+function eheverteilen() {
     if (jaButton.checked) {
-        ehe.style.display = "block";
-    }
-    else {
-        ehe.style.display = "none";
+        let eheg = sessionStorage.getItem('ehePartner');
+
+        let container = document.getElementById("platzehe");
+        container.innerHTML = "";
+        if (eheg == null) {
+            container.innerHTML = "<p>kein Ehegatte vorhanden!</p>";
+        } else {
+            container.innerHTML += '<label >' + eheg + '<input class="form-control inputkind" type="number" id="eheg" name="kindverteilen" oninput="count()" />%' +
+                '</label><br />'
+        }
     }
 
 }
+
 
 
 let ja2Button = document.getElementById("ja2");
@@ -24,13 +29,25 @@ let kind = document.getElementById("kind");
 
 
 kindverteilen();
-function kindverteilen(){
+function kindverteilen() {
     if (ja2Button.checked) {
-        kind.style.display = "block";
+        let anzahl = sessionStorage.getItem('anzahlderkinder');
+
+        let container = document.getElementById("platzkinder");
+
+
+        container.innerHTML = "";
+        if (anzahl == null) {
+            container.innerHTML = "<p>keine Kinder vorhanden!</p>";
+        } else {
+            for (i = 1; i <= anzahl; i++) {
+                let helper = "kind" + i;
+
+                container.innerHTML += '<label>' + sessionStorage.getItem(helper) + '<input class="form-control inputkind" type="number"  id="kind' + i + '" name="kindverteilen" oninput="count()" />% </label><br />'; //new
+            }
+        }
     }
-    else {
-        kind.style.display = "none";
-    }
+
 }
 
 let javButton = document.getElementById("jav");
@@ -41,7 +58,7 @@ let indiv = document.getElementById("indivVert");
 
 
 verteilen();
-function verteilen(){
+function verteilen() {
     if (neinvButton.checked) {
         indiv.style.display = "block";
     }
@@ -50,25 +67,59 @@ function verteilen(){
     }
 }
 
-function addFields(){
-    let anzahl =3 // document.getElementById("anzahlKind").value;
+function count() {
+    let boxList = document.getElementsByName('kindverteilen');
 
-    let container = document.getElementById("platzkinder");
+    let result = 0;
 
-    container.innerHTML = "";
-    for (i = 1; i <= anzahl; i++) {
-        container.innerHTML += '<label id="kind" onchange="addFields()" onclick="addFields()">Name Kind:  <input class="form-control inputkind" type="text" name="kindverteilen" />% </label><br />'; //new
+    for (let index = 0; boxList.length > index; index++) {
+        let boxValue = parseInt(boxList[index].value) || 0;
+        result = result + boxValue;
     }
+
+    document.getElementById('result').innerHTML = result;
 }
 
-function nullFields() {
+function ueberpruefung() {
 
-    let container = document.getElementById("platzkinder");
+    let ehe = document.getElementById('eheg');
+    let counter = document.getElementById('result');
+    let anzahlKinder = sessionStorage.getItem('anzahlderkinder');
+    sessionStorage.removeItem('Anteil')
+    if (javButton.checked == false && neinvButton.checked == false) {
+        window.alert("Bitte treffen Sie eine Auswahl");
+    } else {
+        if (neinvButton.checked) {
+            if ((ja2Button.checked == false && nein2Button.checked == false) || (jaButton.checked == false && neinButton.checked == false)) {
+                window.alert("Bitte treffen Sie für Ihren Ehepartner und für ihre Kinder eine Auswahl");
+            } else {
+                if (counter.innerHTML != 100) {
+                    window.alert("Bitte überprüfen Sie die Verteilung! Es müssen 100% verteilt werden.")
+                } else {
+                    if (jaButton.checked) {
 
-    container.innerHTML = "";
-}
+                        sessionStorage.AnteilEhePartner = ehe.value;
+                    }
+                    if (ja2Button.checked) {
+                        for (var i = 1; i <= anzahlKinder; i++) {
+                            let helper = 'AnteilKind' + i;
+                            sessionStorage.setItem(helper, document.getElementById('kind' + i).value);
+                        }
+                    }
+                    self.location.href = "../html/Zusammenfassung.html"
+                }
+            }
+        } else {
+            sessionStorage.AnteilEhePartner = 50;
 
-function ueberpruefung(){
 
-    self.location.href = "../html/Zusammenfassung.html"
+            var anteilKind = 50 / anzahlKinder;
+            for (var i = 0; i <= anzahlKinder; i++) {
+                let helper = 'AnteilKind' + i;
+                sessionStorage.setItem(helper, anteilKind);
+                self.location.href = "../html/Zusammenfassung.html";
+
+            }
+        }
+    }
 }
